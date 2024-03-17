8430737b36169ae170a0f9639c52e992644ac877
njha <njha@bouyguestelecom.fr>
2024-03-17 18:40:15 +0100

    Implementing Menu Icon Press and Opening in a Dialog

--- app/src/main/java/com/niteshkumarjha/internetphotosearch/MainActivityUI.kt
+++ app/src/main/java/com/niteshkumarjha/internetphotosearch/MainActivityUI.kt
@@ -2,6 +2,7 @@ package com.niteshkumarjha.internetphotosearch
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,9 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
@@ -23,30 +27,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement.Absolute.Center
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.compose.ui.window.Dialog
 
 @Composable
 fun MainActivityUI(openSearchActivity: (String) -> Unit) {
     var searchText by remember { mutableStateOf(TextFieldValue("")) }
+    var isMenuOpen by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopStart
     ) {
         IconButton(
-            onClick = { /*menu icon click*/ }, modifier = Modifier.padding(16.dp)
+            onClick = { isMenuOpen = true }, modifier = Modifier.padding(16.dp)
         ) {
             Icon(Icons.Default.Menu, contentDescription = "Menu")
         }
@@ -114,5 +116,57 @@ fun MainActivityUI(openSearchActivity: (String) -> Unit) {
                 .fillMaxSize()
                 .padding(top = 300.dp)
         )
+
+        if (isMenuOpen) {
+            MenuDialog(onDismiss = { isMenuOpen = false })
+        }
     }
 }
+
+@Composable
+fun MenuDialog(onDismiss: () -> Unit) {
+    Dialog(
+        onDismissRequest = { onDismiss() }
+    ) {
+        Surface(
+            color = Color(0xAA000000),
+            modifier = Modifier.fillMaxSize(),
+            contentColor = Color.White,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MenuItem(text = "My Gallery")
+                MenuItem(text = "Login")
+                MenuItem(text = "Learn More")
+                MenuItem(text = "About")
+                MenuItem(text = "Links")
+            }
+        }
+    }
+}
+
+@Composable
+fun MenuItem(text: String) {
+    Spacer(modifier = Modifier.height(8.dp))
+    Button(
+        onClick = { /* Item click */ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(vertical = 4.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray)
+    ) {
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            color = Color.White
+        )
+    }
+}
+
